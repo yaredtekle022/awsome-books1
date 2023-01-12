90c2200f272c1f0fc8b7d1367ad247349387d423
cindykandie <cindyjk6@gmail.com>
2023-01-12 23:17:16 +0300

    adding js cod to add functionality on main parts of the page

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Book Class:
+class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+  
+  // store classes: handles Storage
+  
+  class Store {
+    static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(author) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if (book.author === author) {
+          books.splice(index, 1);
+        }
+      });
